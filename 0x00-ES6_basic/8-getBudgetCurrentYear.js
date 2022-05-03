bc030ffcfb23d3d286b7926c4460e6c0c7d79ebe
tsims115 <tsims115@yahoo.com>
2022-05-02 20:12:30 -0700

    taking out comment

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,6 +5,7 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   return {
+
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita
