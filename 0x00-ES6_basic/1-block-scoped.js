460dfa2d974f7eaa0212d49d0798e7b79b8cdbb6
tsims115 <tsims115@yahoo.com>
2022-05-02 18:42:52 -0700

    making everything const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    const task = true;
+    const task2 = false;
   }
-  var task = false;
-  var task2 = true;
 
   return [task, task2];
 }
