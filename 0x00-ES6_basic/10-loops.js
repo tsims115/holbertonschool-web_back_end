8b771bea6e034fdbd13a598b92425b4e888cc091
tsims115 <tsims115@yahoo.com>
2022-05-02 20:56:16 -0700

    Adding 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newList = []
+  let value
   for (const idx of array) {
-    let value = idx;
+    value = idx;
     value = appendString + idx;
     newList.push(value)
   }
