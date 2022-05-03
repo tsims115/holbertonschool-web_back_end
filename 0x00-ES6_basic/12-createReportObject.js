545fbc6cdd234b14eb6f9c91a0901dad769aa0f0
tsims115 <tsims115@yahoo.com>
2022-05-02 21:19:53 -0700

    Adding task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
-    getNumberOfDepartments(allEmp) {
+    getNumberOfDepartments() {
       return Object.keys(this.allEmployees).length;
     },
   };
