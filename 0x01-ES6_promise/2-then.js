2836138a2ab565c3d631f4d5f069063f4a193b2c
tsims115 <tsims115@yahoo.com>
2022-05-04 15:06:41 -0700

    fixing style

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-    return promise.then(
-        function(result) {
-            console.log('Got a response form the API');
-            return {
-                status: 200,
-                body: 'success',
-            }
-        },
-        function(error) {
-            return new Error()
-        },
-        );
+  console.log('Got a response form the API');
+  return promise
+    .then(
+      () => ({
+        status: 200,
+        body: 'success',
+      }),
+    )
+    .catch(() => new Error());
 }
