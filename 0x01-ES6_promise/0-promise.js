df4dfb594693fca97e7fcffae6c5fc788df9b548
tsims115 <tsims115@yahoo.com>
2022-05-04 14:41:58 -0700

    Fixing style

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
 export default function getResponseFromAPI() {
   const myPromsie = new Promise((resolve, reject) => {
-
+    resolve('resolved');
+    reject(Error('Error'));
   });
   return myPromsie;
 }
