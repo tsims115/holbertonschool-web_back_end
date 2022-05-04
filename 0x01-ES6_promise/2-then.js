e01cd0c357356ee85e8aa29c3f7c74cc24696d91
tsims115 <tsims115@yahoo.com>
2022-05-04 15:44:26 -0700

    Adding task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   console.log('Got a response form the API');
   return promise
-    .then(
-      () => ({
-        status: 200,
-        body: 'success',
-      }),
-    )
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error());
 }
