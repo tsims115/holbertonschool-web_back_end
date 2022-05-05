9dcd80343a6b7278c3c4a7dc1209910f84befede
tsims115 <tsims115@yahoo.com>
2022-05-04 22:57:47 -0700

    Adding task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([await signUpUser(firstName, lastName), await uploadPhoto(fileName)]);
+  return await signUpUser(firstName, lastName);
 }
