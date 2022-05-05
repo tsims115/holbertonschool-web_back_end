ca05bc2d7069f8f4175656da211cfd302258259a
tsims115 <tsims115@yahoo.com>
2022-05-04 23:54:35 -0700

    Adding task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const settledPromises = await Promise.allSettled([signUpUser(firstName), uploadPhoto(fileName)]).then((results) => results);
-  return settledPromises;
+export default function handleProfileSignUp(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  async function promiseFunct() {
+    const settlesPromises = await Promise.allSettled([userPromise, photoPromise]).then((results) => results);
+    return settlesPromises;
+  }
+  return promiseFunct();
 }
