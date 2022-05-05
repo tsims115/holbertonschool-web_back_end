c49661442da419db230e71a06519a2f6c8c1186f
tsims115 <tsims115@yahoo.com>
2022-05-05 00:32:33 -0700

    Fixig style

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const settledPromises = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  .then((results) => results);
-  const obj = settledPromises[1]
+  const settledPromises = await Promise.allSettled(
+    [signUpUser(firstName, lastName), uploadPhoto(fileName)],
+  )
+    .then((results) => results);
+  const obj = settledPromises[1];
   obj.reason = obj.reason.toString();
-  obj['value'] = obj['reason'];
-  delete obj['reason'];
+  obj.value = obj.reason;
+  delete obj.reason;
   return settledPromises;
 }
