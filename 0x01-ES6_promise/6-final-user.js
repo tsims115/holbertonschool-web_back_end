ad2880df0f005c9e699dfde95a3e3fc8275956ca
tsims115 <tsims115@yahoo.com>
2022-05-04 23:33:51 -0700

    Adding task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,22 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return [
     await signUpUser(firstName, lastName)
-      .then((response) => ({
-        status: 'fulfilled',
-        response,
-      }))
-      .catch((response) => ({
-        status: 'rejected',
-        value: response,
-      })),
+      .then((response) => ({ status: 'fulfilled', response }))
+      .catch((response) => ({ status: 'rejected', value: response })),
     await uploadPhoto(fileName)
-      .then((response) => ({
-        status: 'fulfilled',
-        response,
-      }))
-      .catch((response) => ({
-        status: 'rejected',
-        value: response.toString(),
-      })),
-  ];
+      .then((response) => ({ status: 'fulfilled', response }))
+      .catch((response) => ({ status: 'rejected', value: response.toString() }))];
 }
