21444f3ca21c9604146a9310b3d2c019fcc4f78b
tsims115 <tsims115@yahoo.com>
2022-05-04 21:50:32 -0700

    Adding task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,21 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto()
-    .then(
-      (result) => { process.stdout.write(result.body); },
-    )
-    .catch(
-      () => { console.log('Signup system offline'); },
-    );
-  createUser()
-    .then(
-      (result) => {
-        process.stdout.write(` ${result.firstName} `);
-        process.stdout.write(`${result.lastName}\n`);
-      },
-    )
-    .catch(
-      () => { console.log('Signup system offline'); },
-    );
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
