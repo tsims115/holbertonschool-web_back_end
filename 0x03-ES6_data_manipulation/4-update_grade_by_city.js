4c637bf25e456c1070b7712d0e22f7489f88582d
tsims115 <tsims115@yahoo.com>
2022-05-11 07:47:22 -0700

    Changing parameter name

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-  studentList = studentList.filter((s) => s.location === city);
-  return studentList.map((s) => {
+  const newList = studentList.filter((s) => s.location === city);
+  return newList.map((s) => {
     let grade = newGrades.filter((g) => g.studentId === s.id);
     if (grade.length === 0) {
       grade = 'N/A';
