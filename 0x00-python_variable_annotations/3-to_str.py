a51d77716cb698271e5cd02f7a54917e3f009f83
Trenton Sims <tsims115@yahoo.com>
2022-08-29 18:37:15 -0500

    Adding docs for all

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -2,6 +2,6 @@
 """to_str takes a float and converts to a string representation"""
 
 
-def to_str(n:float) -> str:
+def to_str(n: float) -> str:
     """to_str takes a float and converts to a string representation"""
     return str(n)
