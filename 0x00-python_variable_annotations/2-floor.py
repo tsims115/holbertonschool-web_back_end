2ffdd83a472335188bf28949f1874baee494834a
Trenton Sims <tsims115@yahoo.com>
2022-08-29 18:35:31 -0500

    Adding docs for all

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """floor takes a float and returns the floor of the float"""
 
-
 import math
+
 
 def floor(n:float) -> int:
     """floor takes a float and returns the floor of the float"""
