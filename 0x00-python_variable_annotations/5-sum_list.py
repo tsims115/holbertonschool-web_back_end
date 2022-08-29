6a2e4b5e7c2dce8edc5a1f677c421d98c8bea3f3
Trenton Sims <tsims115@yahoo.com>
2022-08-29 18:37:49 -0500

    Adding docs for all

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-# defines annotations
+"""defines annotations"""
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
+    """defines annotations"""
     sum: float
     sum = 0
     for i in input_list:
