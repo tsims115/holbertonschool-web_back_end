09fa499cea9dc9ecb2933f9f38b8ff98abb22037
Trenton Sims <tsims115@yahoo.com>
2022-08-29 02:02:24 -0500

    Adding tasks 6 and 7

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# mixed list
+
+from typing import List, Union
+
+types = Union[int, float]
+
+def sum_mixed_list(mxd_lst: List[types]) -> float:
+    sum: float
+    sum = 0
+    for i in mxd_lst:
+        sum += i
+    return sum
---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# string and int/float
+
+from typing import List, Union, Tuple
+
+itypes = Union[int, float]
+ttypes = Union[str, float]
+
+def to_kv(k: str, v: itypes) -> Tuple[str, float]:
+    return (k, v * v)
