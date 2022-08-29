96b33287822b643f0cdeb2cee69a923849b156ec
Trenton Sims <tsims115@yahoo.com>
2022-08-29 18:40:00 -0500

    Adding docs

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-# string and int/float
+"""string and int/float"""
 
 from typing import List, Union, Tuple
 
 itypes = Union[int, float]
 ttypes = Union[str, float]
+
 
 def to_kv(k: str, v: itypes) -> Tuple[str, float]:
     return (k, v * v)
