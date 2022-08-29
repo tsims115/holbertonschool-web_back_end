186c049e23714575072cb071d9808b1805edb9e6
Trenton Sims <tsims115@yahoo.com>
2022-08-29 18:50:07 -0500

    Adding docs

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""multiplies by multiplier"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """multiplies by multiplier"""
+    def multi(num: float) -> float:
+        """returns the multiplied numbered"""
+        return num *  num
+    return multi
