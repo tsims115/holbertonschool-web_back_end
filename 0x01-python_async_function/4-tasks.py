0cd07b15573dfd8126c5ea3c901a4f5e0e1df1a2
Trenton Sims <tsims115@yahoo.com>
2022-08-30 18:54:28 -0500

    Adding task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""take two ints and calls wait_random"""
+
+from random import uniform
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n, max_delay: int) -> List[float]:
+    """ calls task_wait_random """
+    delay_list = [task_wait_random(max_delay) for i in range(n)]
+    wait_times = []
+    for item in asyncio.as_completed(delay_list):
+        wait_times.append(await item)
+    return wait_times
