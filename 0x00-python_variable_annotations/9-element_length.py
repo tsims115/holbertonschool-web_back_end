c0d3d40a3f9b6a8141699b63b801b1f98d24b352
Trenton Sims <tsims115@yahoo.com>
2022-08-29 19:01:33 -0500

    adding task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,6 +4,6 @@
 from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]] :
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """elements lengths returned"""
     return [(i, len(i)) for i in lst]
