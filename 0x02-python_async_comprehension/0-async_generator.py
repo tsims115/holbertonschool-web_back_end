5132c0361c2568227b019fe5edc459a7aa249a32
Trenton Sims <tsims115@yahoo.com>
2022-08-31 18:57:24 -0500

    Adding task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ from random import uniform
 from typing import Generator
 import asyncio
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """ async generator """
     for i in range(10):
         await asyncio.sleep(1)
