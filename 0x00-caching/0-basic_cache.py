53cebd5f9999883d62fee399d42b599a3798edad
Trenton Sims <tsims115@yahoo.com>
2022-09-06 12:33:20 -0500

    Adding task 0

---
+++ 0x00-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""caching system inherits from BaseCaching"""
+
+BaseCaching = __import__('baseCaching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """inherits from BaseCaching"""
+    def put(self, key, item):
+        """Add an item to cache"""
+        if key == None or item == None:
+            return None
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key == None:
+            return None
+        if key in self.cache_data:
+            return self.cache_data[key]
