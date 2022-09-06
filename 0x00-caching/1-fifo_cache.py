2651b92582ca265510af411d84c0e966fa85c5e1
Trenton Sims <tsims115@yahoo.com>
2022-09-06 13:05:28 -0500

    Adding task 1

---
+++ 0x00-caching/1-fifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""creates FIFO class that inherits from BaseCaching"""
+
+BaseCaching = __import__('baseCaching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO class"""
+    def put(self, key, item):
+        """Add an item to cache in last position"""
+        if key == None or item == None:
+            return None
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            for k, v in self.cache_data.items():
+                del self.cache_data[k]
+                break
+
+    def get(self, key):
+        """Get an item by key"""
+        if key == None:
+            return None
+        if key in self.cache_data:
+            return self.cache_data[key]
+            
