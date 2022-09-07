70cca78912478ce76f8c428595960b67efa769d2
Trenton Sims <tsims115@yahoo.com>
2022-09-07 11:59:06 -0500

    Adding task 2

--- 0x00-caching/2-lifo_cache.py
+++ 0x00-caching/2-lifo_cache.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 """creates LIFO class that inherits from BaseCaching"""
 
-BaseCaching = __import__('baseCaching').BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
     """LIFO Class"""
+
+    def __init__(self):
+        """init method calls super"""
+        super().__init__()
+        self.last = None
+
     def put(self, key, item):
         """Add an item to cache in last position"""
         if key is None or item is None:
             return None
+        if key in self.cache_data.keys():
+            self.cache_data[key] = item
+            print("DISCARD: " + key)
+            self.last = key
+            return
         if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-            print("Discard: ")
-            i = 1
-            for k, v in self.cache_data.items():
-                if i == len(self.cache_data):
-                    tmp_key = k
-                i += 1
-            del self.cache_data[tmp_key]
+            del self.cache_data[self.last]
+            print("DISCARD: " + self.last)
         self.cache_data[key] = item
+        self.last = key
 
     def get(self, key):
         """Get an item by key"""
