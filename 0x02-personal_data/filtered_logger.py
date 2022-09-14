e87f9735239980381440ff81f35f6b960e704b8e
Trenton Sims <tsims115@yahoo.com>
2022-09-14 00:15:53 -0500

    Adding filtered logging

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 """filter datum obfuscated"""
 from typing import List
-from re import sub, split, search
+import re
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[int]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters log and formats it"""
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        record.msg = '; '.join(record.msg.split(';'))
+        formatter = logging.Formatter(self.FORMAT)
+        return formatter.format(record)
 
 def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
     """Filters and rerturns obfuscated data"""
     for f in fields:
-        tmp = split(seperator, message[search(f, message).span()[1] + 1:])
-        message = sub(tmp[0], redaction, message)
+        tmp = re.split(seperator, message[re.search(f, message).span()[1] + 1:])
+        message = re.sub(tmp[0], redaction, message)
     return message
