5eff6a54695fc658f679979e5606cccc88770ae9
tsims115 <tsims115@yahoo.com>
2022-09-14 18:43:48 -0700

    Adding task 5

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf8'), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks password"""
+    return bcrypt.checkpw(password.encode('utf8'), hashed_password)
