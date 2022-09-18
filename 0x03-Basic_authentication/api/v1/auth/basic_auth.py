b3159f63216271c4f3703ff7f7fdf4387ea0bd59
tsims115 <tsims115@yahoo.com>
2022-09-18 13:38:49 -0700

    Taking out basic auth

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from api.v1.auth.auth import Auth
-"""basic_auth class inherits from Auth and is not made"""
-
-
-class BasicAuth(Auth):
-    """BasicAuth class that is Nothing for now"""
-    pass
