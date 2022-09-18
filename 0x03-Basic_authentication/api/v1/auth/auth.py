ac1ab9ea5c9520d9615fe0974a4602ca8e813407
tsims115 <tsims115@yahoo.com>
2022-09-18 12:23:59 -0700

    Adding task for 0-4

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -2,17 +2,28 @@
 """ holds auth class """
 from flask import request
 from typing import TypeVar, List
+import re
 
 class Auth:
     """ Auth class """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Implement later"""
-        return False
+        """ checks the current list of paths """
+        if excluded_paths is None or path is None:
+            return True
+        for p in excluded_paths:
+            path_regex = re.compile(f'{p[:-1]}/?')
+            if path_regex.search(path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """Implement later"""
-        return None
+        """checks for Authorization header key"""
+        if request is None:
+            return None
+        # Must implment header check later
+        print(request)
+        return request
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Implement later"""
