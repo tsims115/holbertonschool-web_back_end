b13713aae0108dd1a8e76840fe9d069a2bc86ebb
tsims115 <tsims115@yahoo.com>
2022-09-26 09:31:51 -0700

    Adding task 14

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -58,7 +58,7 @@ def logout():
             session_id = cookie[1]
         user = AUTH.get_user_from_session_id(session_id)
         if user is None:
-            return 403
+            return abort(403)
         AUTH.destroy_session(session_id)
         return redirect(url_for('simple'), code=32)
 
