8702ca3fff20c71a47eedf05fbe4a231598e3fcb
tsims115 <tsims115@yahoo.com>
2022-09-26 06:42:50 -0700

    Adding task 13

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -63,3 +63,14 @@ class Auth:
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys session based on user_id"""
+        if user_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+        except NoResultFound:
+            return None
+        user.session_id = None
+        return None
