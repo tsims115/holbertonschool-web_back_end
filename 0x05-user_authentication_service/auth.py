844c6a11f74d14fab1d2e2629e9e6ddc80d8fde1
tsims115 <tsims115@yahoo.com>
2022-09-25 22:26:15 -0700

    Adding task 4

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -4,8 +4,6 @@
 from bcrypt import hashpw, gensalt
 
 
-def _hash_password(self, password: str) -> bytes:
+def _hash_password(password: str) -> bytes:
         """Hashes and salts the password"""
-        salt = gensalt()
-        hashed = hashpw(password.encode('utf8'), salt)
-        return hashed
+        return hashpw(password.encode('utf8'), gensalt())
