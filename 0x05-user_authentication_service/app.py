c77ec120a557994964d4d3190f39a74edb30304d
tsims115 <tsims115@yahoo.com>
2022-09-26 10:35:34 -0700

    Adding task 14

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Route module for the API
 """
 from os import getenv
-from flask import Flask, jsonify, abort, request, make_response
+from flask import Flask, jsonify, abort, request, make_response, redirect
 from flask_cors import (CORS, cross_origin)
 import os
 from auth import Auth
