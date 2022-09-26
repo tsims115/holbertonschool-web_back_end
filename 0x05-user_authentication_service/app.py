9451f848e9b2af2f9aca7882b539e3ce4b62000b
tsims115 <tsims115@yahoo.com>
2022-09-26 00:19:54 -0700

    Adding task 11

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Route module for the API
 """
 from os import getenv
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, make_response
 from flask_cors import (CORS, cross_origin)
 import os
 from auth import Auth
@@ -27,6 +27,21 @@ def users():
         return jsonify({"message": "email already registered"}), 400
     m = {"email": "<registered email>", "message": "user created"}
     return jsonify(m), 200
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /
+    Return:
+      - Json message verifying status
+    """
+    email = list(request.form)[0]
+    password = list(request.form)[1]
+    if AUTH.valid_login(email, password):
+        sid = AUTH.create_session(email)
+        resp = make_response()
+        resp.set_cookie('session_id', sid)
+        return jsonify({"email": "<user email>", "message": "logged in"})
+    abort(401)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
