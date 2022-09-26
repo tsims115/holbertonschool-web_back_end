73250690c6ac1d7376f9628bcb8eacc095ff675e
tsims115 <tsims115@yahoo.com>
2022-09-25 22:05:58 -0700

    Fixing task 2 annotation

--- 0x05-user_authentication_service/db.py
+++ 0x05-user_authentication_service/db.py
@@ -38,7 +38,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: User) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """Finds the user"""
         user = self._session.query(User).filter_by(**kwargs).first()
         k = kwargs.keys()
