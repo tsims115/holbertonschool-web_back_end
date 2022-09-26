aff17c0d5e6c2a3877b195b8623c0bdef1f51dcb
tsims115 <tsims115@yahoo.com>
2022-09-25 23:22:32 -0700

    Adding task 7

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -6,9 +6,23 @@ from os import getenv
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """POST"""
+    email = list(request.form)[0]
+    password = list(request.form)[1]
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": "<registered email>", "message": "user created"}), 200
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
