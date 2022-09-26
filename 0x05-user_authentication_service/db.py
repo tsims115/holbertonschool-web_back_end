e8f535b1dacd5e26c0f7c705e5f69de5f0c2e9e1
tsims115 <tsims115@yahoo.com>
2022-09-25 21:59:49 -0700

    Adding tasks 1-3

---
+++ 0x05-user_authentication_service/db.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Creates a new user"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs: User) -> User:
+        """Finds the user"""
+        user = self._session.query(User).filter_by(**kwargs).first()
+        k = kwargs.keys()
+        f = list(kwargs.keys())[0]
+        if f not in dir(User):
+            raise InvalidRequestError()
+        if user == None:
+            raise NoResultFound()
+        return use
---
+++ 0x05-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Creates sqlalchemy model named User"""
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+from sqlalchemy import Column, Integer, String
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250),  nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+       return "<User(name="
