347326bc5218a814060b081893c66f75ba09427c
tsims115 <tsims115@yahoo.com>
2022-09-25 22:56:00 -0700

    Adding task 6

---
+++ 0x05-user_authentication_service/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+
+@app_views.route('/', methods=['GET'], strict_slashes=False)
+def simple() -> str:
+    """ GET /
+    Return:
+      - Simple Message
+    """
+    return flask.jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
