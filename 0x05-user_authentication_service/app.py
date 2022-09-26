a4b8a26eae21f3d02b705b55bc704ab7867a50b7
tsims115 <tsims115@yahoo.com>
2022-09-25 22:57:23 -0700

    Adding task 6

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -12,7 +12,7 @@ import os
 app = Flask(__name__)
 
 
-@app_views.route('/', methods=['GET'], strict_slashes=False)
+@route('/', methods=['GET'], strict_slashes=False)
 def simple() -> str:
     """ GET /
     Return:
