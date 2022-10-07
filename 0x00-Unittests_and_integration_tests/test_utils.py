3229a631bcfe7475aac5259359172eb5526606f0
tsims115 <tsims115@yahoo.com>
2022-10-07 07:51:40 -0700

    Adding task 0

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -4,12 +4,17 @@
 import inspect
 import pep8
 import unittest
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test the access map utility"""
 
-    @parameterized.expand
-    def test_access_nested_map(self):
-        """Tests the accessnested map function"""
-        self.assertEqual()
+    @parameterized.expand([
+        ({'a': 1}, ('a'), 1),
+        ({'a': {'b': 2}}, ('a'), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Tests the access nested map function"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
