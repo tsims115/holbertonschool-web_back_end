a4dcfb650dd301ec4dcc019886e29c547e18c18a
tsims115 <tsims115@yahoo.com>
2022-10-10 13:23:51 -0700

    Adding task 1

---
+++ 0x01-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" task 0 flask App """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+ 
+class Config:
+    """Babel configuration"""
+    LANGUAGES = ['en', 'fr']
+
+Config.BABEL_DEFAULT_LOCALE = "en"
+Config.BABEL_DEFAULT_TIMEZONE = "UTC"
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """outputs h1 hello world html template"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
