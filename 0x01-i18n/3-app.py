b6903e2444062a9f9dbadc92df52b0a437841ee6
tsims115 <tsims115@yahoo.com>
2022-10-10 18:06:47 -0700

    Adding task 3

---
+++ 0x01-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" task 0 flask App """
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+
+class Config:
+    """Babel configuration"""
+    LANGUAGES = ['en', 'fr']
+
+
+Config.BABEL_DEFAULT_LOCALE = "en"
+Config.BABEL_DEFAULT_TIMEZONE = "UTC"
+app.config.from_object(Config)
+_('home_title')
+_('home_header')
+
+
+@babel.localeselector
+def get_locale():
+    """gets the current locale"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """outputs h1 hello world html template"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
