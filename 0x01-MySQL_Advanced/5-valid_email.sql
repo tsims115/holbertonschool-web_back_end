90e40bf71774eee9e00287691dac9e3a3422333d
tsims115 <tsims115@yahoo.com>
2022-10-13 15:09:46 -0700

    Adding task 5

--- 0x01-MySQL_Advanced/5-valid_email.sql
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -4,7 +4,9 @@ CREATE TRIGGER v_email AFTER UPDATE of email ON users
 FOR EACH ROW
 BEGIN
 if email = OLD.email
+then
     UPDATE users
     SET valid_email = 1
-    WHERE email = NEW.email;
+    WHERE email = NEW.email
+END IF;
 END $$;
