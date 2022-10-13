73f7149a2b35b5c0b7f5ffc76e6def67b01b1c1f
tsims115 <tsims115@yahoo.com>
2022-10-13 15:03:43 -0700

    Adding task 5

---
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- creates a trigger that resets email attribute
+DELIMITER $$
+CREATE TRIGGER v_email AFTER UPDATE of email ON users
+FOR EACH ROW
+BEGIN
+    UPDATE users
+    SET valid_email = 1
+    WHERE email = NEW.email;
+END $$;
