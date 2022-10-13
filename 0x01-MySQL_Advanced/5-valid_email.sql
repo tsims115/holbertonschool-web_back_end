d75fed1af8596198a890c8a0109293c550510976
tsims115 <tsims115@yahoo.com>
2022-10-13 15:24:45 -0700

    Adding task 5

--- 0x01-MySQL_Advanced/5-valid_email.sql
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -3,9 +3,7 @@ DELIMITER $$
 CREATE TRIGGER v_email AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-IF (NEW.email <=> OLD.email) THEN
-    UPDATE users
+IF (NEW.email != OLD.email) THEN
     SET NEW.valid_email = 0
-    WHERE email = NEW.email
 END IF;
 END $$;
