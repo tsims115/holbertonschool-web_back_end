743fbb6703b0072967f5df3de5daeff56e503484
tsims115 <tsims115@yahoo.com>
2022-10-13 10:26:44 -0700

    ADding task 0

--- 0x01-MySQL_Advanced/0-uniq_users.sql
+++ 0x01-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --- creates table of user with id, email, name
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
-    email varchar(250) unique NOT NULL,
+    email varchar(250) UNIQUE NOT NULL,
     name varchar(250),
     PRIMARY KEY (id)
 );
