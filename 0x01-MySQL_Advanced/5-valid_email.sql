ba2d770441b3cbc4d66cef6956f31ee7259d1d6e
tsims115 <tsims115@yahoo.com>
2022-10-13 15:38:07 -0700

    Adding task 5

--- 0x01-MySQL_Advanced/5-valid_email.sql
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,6 @@
 -- creates a trigger that resets email attribute
 DELIMITER $$
-CREATE TRIGGER v_email AFTER UPDATE ON users
+CREATE TRIGGER v_email BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 IF NEW.email != OLD.email THEN
