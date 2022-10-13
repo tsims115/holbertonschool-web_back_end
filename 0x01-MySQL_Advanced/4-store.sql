ee03eb4d5b26b77b52796b81274e24be76e6e6ab
tsims115 <tsims115@yahoo.com>
2022-10-13 14:53:21 -0700

    Adding task 4

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -1,7 +1,9 @@
 -- Creates a trigger that updates the table items
+DELIMITER $$
 CREATE TRIGGER ins_sum AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
 UPDATE items
 SET quantity = quantity - NEW.number
 WHERE NEW.item_name = name;
+END $$;
