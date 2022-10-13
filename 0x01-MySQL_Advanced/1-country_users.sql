d1b4a4fa3280936c417b2e8006b59510cb48b438
tsims115 <tsims115@yahoo.com>
2022-10-13 10:47:32 -0700

    ADding task 1

--- 0x01-MySQL_Advanced/1-country_users.sql
+++ 0x01-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- creates table user
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     PRIMARY KEY (id),
     id int AUTO_INCREMENT NOT NULL,
     email varchar(250) NOT NULL UNIQUE,
     name varchar(250),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-) IF NOT EXISTS;
+);
