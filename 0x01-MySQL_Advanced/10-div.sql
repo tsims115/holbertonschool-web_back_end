620a35233e24497d22604eb155c1097b8f2cc24e
tsims115 <tsims115@yahoo.com>
2022-10-14 00:05:36 -0700

    Adding task 10

--- 0x01-MySQL_Advanced/10-div.sql
+++ 0x01-MySQL_Advanced/10-div.sql
@@ -1,8 +1,8 @@
 -- Creates funstion SafeDiv that divides first by second number
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
-CREATE FUNCTION SafeDiv(n1 int, n2 int)
-RETURNS int;
+CREATE FUNCTION SafeDiv (n1 int, n2 int)
+RETURNS FLOAT DETERMINISTIC;
     IF n2 = 0 THEN
     RETURN 0;
     ELSE
