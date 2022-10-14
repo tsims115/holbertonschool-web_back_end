1ce5543179aa970440c83b4332764f5592f3088e
tsims115 <tsims115@yahoo.com>
2022-10-13 21:50:47 -0700

    Adding task 7

--- 0x01-MySQL_Advanced/7-average_score.sql
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -5,8 +5,8 @@ CREATE PROCEDURE ComputeAverageScoreForUser (
     )
 BEGIN
     SET @avg_score :=
-    SELECT AVG(score) FROM corrections WHERE user_id = user_id;
+    SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id;
     UPDATE users
     SET average_score = @avg_score
-    WHERE user_id = user_id;
+    WHERE id = user_id;
 END $$;
