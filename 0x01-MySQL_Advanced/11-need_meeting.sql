bf8bfed35bb7f6431c7be66f03c2f2ff26e335aa
tsims115 <tsims115@yahoo.com>
2022-10-14 00:19:36 -0700

    Adding task 11

--- 0x01-MySQL_Advanced/11-need_meeting.sql
+++ 0x01-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,7 @@
 -- Creates a view need_meeting lists all students with score under 80
 DELIMITER $$
-CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80;
+CREATE VIEW need_meeting AS SELECT name FROM students 
+WHERE score < 80
+AND last_meeting IS NULL
+OR 30 < DATEDIFF(NOW(), last_meeting);
 END $$;
