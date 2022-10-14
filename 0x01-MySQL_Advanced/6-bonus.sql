62a1720e92b009eb426cc7b888cac1aa42eb118d
tsims115 <tsims115@yahoo.com>
2022-10-13 21:24:30 -0700

    ADding task 6

--- 0x01-MySQL_Advanced/6-bonus.sql
+++ 0x01-MySQL_Advanced/6-bonus.sql
@@ -1,15 +1,18 @@
 -- creates a stored procedure
 DELIMITER $$
-CREATE PROCEDURE AddBonus (
+DROP PROCEDURE IF EXISTS AddBonus;
+CREATE PROCEDURE IF NOT EXISTS AddBonus (
     in user_id int,
     in project_name VARCHAR(250),
     in score float
     )
 BEGIN
+DECLARE pid int;
 IF NOT EXISTS (SELECT id FROM projects WHERE name = project_name) THEN
     INSERT INTO projects (name)
     VALUES (project_name);
-ENDIF;
-INSERT INTO corrections (user_id, project_id, score)
-VALUES (user_id, project_name, score);
+END IF;
+SELECT id INTO pid FROM projects WHERE name = project_name;
+INSERT INTO corrections (user_id, pid, score)
+VALUES (user_id, pid, score);
 END $$;
