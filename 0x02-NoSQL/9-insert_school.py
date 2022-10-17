f191d5ef9545ba75148c23d134c57728e3d5266e
tsims115 <tsims115@yahoo.com>
2022-10-17 08:49:45 -0700

    Adding task 9

--- 0x02-NoSQL/9-insert_school.py
+++ 0x02-NoSQL/9-insert_school.py
@@ -5,4 +5,4 @@ from pymongo import MongoClient
 
 def insert_school(mongo_collection, **kwargs):
     """inserts kwargs into mongo_collection"""
-    mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert_one(kwargs)
