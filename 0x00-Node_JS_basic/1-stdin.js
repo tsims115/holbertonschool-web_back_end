95b12d2d7455785dc5a499d5872fde308d3717af
tsims115 <72327718+tsims115@users.noreply.github.com>
2022-10-21 14:37:46 -0500

    Adding task 1

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
-console.log("Welcome to Holberton School, what is your name?");
-let name;
-process.stdin.once('readable', (name) => {
-  name = process.stdin.read()
-  process.stdout.write("Your name is: " + name);
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.once('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is:  + ${name}`);
   console.log('This important software is now closing');
 });
