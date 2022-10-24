83ba1d7a6be854be257c632999b2eb1ff0893a36
Trenton Sims <tsims115@yahoo.com>
2022-10-24 16:24:06 -0500

    Adding task 3

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -25,8 +25,8 @@ module.exports = function countStudents(path) {
         }
         console.log(`Number of students in CS: ${fields.CS.length}. List: ${fields.CS.join(', ')}`);
         console.log(`Number of students in SWE: ${fields.SWE.length}. List: ${fields.SWE.join(', ')}`);
-        resolve(['resolved']);
       }
+      resolve(['resolved']);
     });
   });
 };
