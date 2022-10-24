d2926d6a5ddc976f413867a41afc7f32e2182f79
Trenton Sims <tsims115@yahoo.com>
2022-10-24 15:37:53 -0500

    Adding task 3

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -1,11 +1,31 @@
 const fs = require('fs');
+const { resolve } = require('path');
 
 module.exports = function countStudents(path) {
-  let data;
-  try {
-    data = fs.readFile(path, 'utf8');
-  } catch (error) {
-    throw Error('Cannot load the database');
-  }
-  console.log(data);
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        throw Error('Cannot load the database');
+      }
+      let i;
+      const fields = {};
+      data = data.split('\n');
+      if (data[data.length - 1] === '') {
+        data.pop();
+      }
+      console.log(`Number of students: ${data.length - 1}`);
+      for (i = 1; i < data.length; i += 1) {
+        data[i] = data[i].split(',');
+        const fieldName = data[i][3];
+        if (fields[fieldName] === undefined) {
+          fields[fieldName] = [data[i][0]];
+        } else {
+          fields[fieldName].push(data[i][0]);
+        }
+      }
+      console.log(`Number of students in CS: ${fields.CS.length}. List: ${fields.CS.join(', ')}`);
+      console.log(`Number of students in SWE: ${fields.SWE.length}. List: ${fields.SWE.join(', ')}`);
+      resolve('resolved');
+    });
+  });
 };
