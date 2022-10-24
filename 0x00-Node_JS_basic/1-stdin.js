1c8816d16385da92f12cb1af6832f483ea6b9cbe
Trenton Sims <tsims115@yahoo.com>
2022-10-24 10:24:24 -0500

    Adding task 1

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -2,5 +2,5 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.once('readable', () => {
   const name = process.stdin.read();
   process.stdout.write(`Your name is: ${name}`);
+  console.log('This important software is now closing');
 });
-console.log('This important software is now closing');
