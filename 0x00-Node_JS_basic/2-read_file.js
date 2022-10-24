9bf73788ed67f646daa318e10217a312683d1636
tsims115 <72327718+tsims115@users.noreply.github.com>
2022-10-24 12:19:43 -0500

    Adding task 2

--- 0x00-Node_JS_basic/2-read_file.js
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = function countStudents(path) {
   let data;
   try {
-    data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+    data = fs.readFileSync(path, 'utf8');
   } catch (error) {
     throw Error('Cannot load the database');
   }
