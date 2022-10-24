2ec4aac8b0291ac51731269fbf1dccd87e41fd46
Trenton Sims <tsims115@yahoo.com>
2022-10-24 17:00:30 -0500

    Adding task 4

--- 0x00-Node_JS_basic/4-http.js
+++ 0x00-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('Hello Holberton School!');
   res.end(); //end the response
 });
