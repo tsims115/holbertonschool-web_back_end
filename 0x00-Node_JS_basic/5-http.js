eeb257585311fca28720cb4024791a4eb4b1b1cf
Trenton Sims <tsims115@yahoo.com>
2022-10-24 17:12:37 -0500

    Adding task 5

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -9,7 +9,13 @@ const app = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('This is the list of our students');
     const countStudents = require('./3-read_file_async');
-    countStudents("database.csv");
+    countStudents("database.csv")
+    .then(() => {
+    })
+        .catch((error) => {
+        console.log(error);
+    });
+
   }
   res.end();
 });
