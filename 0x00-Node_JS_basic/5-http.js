099a7fef6637f4838d0ae801af8ea721f690db21
Trenton Sims <tsims115@yahoo.com>
2022-10-24 17:10:09 -0500

    Adding task 5

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -1,11 +1,12 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('Hello Holberton School!');
   }
   if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('This is the list of our students');
     const countStudents = require('./3-read_file_async');
     countStudents("database.csv");
