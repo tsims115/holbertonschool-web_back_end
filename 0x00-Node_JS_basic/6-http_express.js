0f7a3c0b49f0bee9f4ac9e346d26d941933ab86b
Trenton Sims <tsims115@yahoo.com>
2022-10-24 18:11:49 -0500

    Adding task 6

--- 0x00-Node_JS_basic/6-http_express.js
+++ 0x00-Node_JS_basic/6-http_express.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!')
+  res.send('Hello Holberton School!');
 })
 
-app.listen(port)
+app.listen(port);
 
 module.exports = app;
