1d91df62464fce80c32a6781971fc52178dcb666
Trenton Sims <tsims115@yahoo.com>
2022-10-24 16:10:48 -0500

    Adding task 3

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -4,28 +4,29 @@ const { resolve } = require('path');
 module.exports = function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(Error('Cannot load the database'));
-      }
-      let i;
-      const fields = {};
-      const data1 = data.split('\n');
-      if (data1[data1.length - 1] === '') {
-        data1.pop();
-      }
-      console.log(`Number of students: ${data1.length - 1}`);
-      for (i = 1; i < data1.length; i += 1) {
-        data1[i] = data1[i].split(',');
-        const fieldName = data1[i][3];
-        if (fields[fieldName] === undefined) {
-          fields[fieldName] = [data1[i][0]];
-        } else {
-          fields[fieldName].push(data1[i][0]);
+      if (!data) {
+        reject('Cannot load the database');
+      } else {
+        let i;
+        const fields = {};
+        const data1 = data.split('\n');
+        if (data1[data1.length - 1] === '') {
+          data1.pop();
         }
+        console.log(`Number of students: ${data1.length - 1}`);
+        for (i = 1; i < data1.length; i += 1) {
+          data1[i] = data1[i].split(',');
+          const fieldName = data1[i][3];
+          if (fields[fieldName] === undefined) {
+            fields[fieldName] = [data1[i][0]];
+          } else {
+            fields[fieldName].push(data1[i][0]);
+          }
+        }
+        console.log(`Number of students in CS: ${fields.CS.length}. List: ${fields.CS.join(', ')}`);
+        console.log(`Number of students in SWE: ${fields.SWE.length}. List: ${fields.SWE.join(', ')}`);
+        resolve();
       }
-      console.log(`Number of students in CS: ${fields.CS.length}. List: ${fields.CS.join(', ')}`);
-      console.log(`Number of students in SWE: ${fields.SWE.length}. List: ${fields.SWE.join(', ')}`);
-      resolve();
     });
   });
 };
