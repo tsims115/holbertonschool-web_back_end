0ba767206ccd8e2ca1cc678c2410671207d3ee76
Trenton Sims <tsims115@yahoo.com>
2022-10-24 16:53:29 -0500

    Adding task 4

--- 0x00-Node_JS_basic/4-http.js
+++ 0x00-Node_JS_basic/4-http.js
@@ -1,9 +1,8 @@
 const http = require('http');
-const { DefaultSerializer } = require('v8');
 
 app = http.createServer(function (req, res) {
   res.write('Hello Holberton School!'); //write a response to the client
   res.end(); //end the response
 }).listen(1245);
 
-export default app;
+module.exports = app;
