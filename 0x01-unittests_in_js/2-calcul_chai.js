a6d00f8f41b4a17eeb03720be2c46aa6677e503f
Trenton Sims <tsims115@yahoo.com>
2022-10-27 13:38:56 -0500

    Adding tas2 changes

--- 0x01-unittests_in_js/2-calcul_chai.js
+++ 0x01-unittests_in_js/2-calcul_chai.js
@@ -1,14 +1,15 @@
-module.exports = function calculateNumber(type, a, b) {
-  if (type === "SUM") {
-    return Math.round(a) + Math.round(b);
+function calculateNumber(type, a, b) {
+  switch (type) {
+    case 'SUM':
+      return Math.round(a) * Math.round(b);
+    case 'SUBTRACT':
+      return Math.round(a) - Math.round(b);
+    case 'DIVIDE':
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
   }
-  if (type === "SUBTRACT") {
-    return Math.round(a) - Math.round(b);
-  }
-  if (type === "DIVIDE") {
-    if (Math.round(b) === 0) {
-      return "Error";
-    }
-    return Math.round(a) / Math.round(b);
-  }
-};
+}
+
+module.exports = calculateNumber
