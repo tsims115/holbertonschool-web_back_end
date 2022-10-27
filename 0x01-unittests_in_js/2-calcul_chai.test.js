0c50e7f528df3fec92ee4072224716841b471d0d
Trenton Sims <tsims115@yahoo.com>
2022-10-26 22:07:32 -0500

    Adding task 2

--- 0x01-unittests_in_js/2-calcul_chai.test.js
+++ 0x01-unittests_in_js/2-calcul_chai.test.js
@@ -5,24 +5,17 @@ const expect = chai.expect;
 describe('calculateNumber', function () {
   it('Testing positive numbers and non-capital letters', () => {
 		expect(calculateNumber('SUM', 2, 2)).to.equal(4);
-		expect(calculateNumber('divide', 2, 2)).to.equal(1);
-		expect(calculateNumber('SubTract', 2, 2)).to.equal(0);
 		expect(calculateNumber('SUM', 1.5, 1.5)).to.equal(4);
 		expect(calculateNumber('SUM', 1.5, 2)).to.equal(4);
 		expect(calculateNumber('SUM', 2, 1.5)).to.equal(4);
 	});
 	it('Testing negative numbers and non-capital letters', () => {
 		expect(calculateNumber('SUM', -2, -2)).to.equal(-4);
-		expect(calculateNumber('suBtrAct', -2, -6)).to.equal(4);
-		expect(calculateNumber('divide', -16, -4)).to.equal(4);
 		expect(calculateNumber('SUM', -2.5, -2.5)).to.equal(-4);
 		expect(calculateNumber('SUM', -2.5, -2)).to.equal(-4);
 		expect(calculateNumber('SUM', -2, -2.5)).to.equal(-4);
 	});
 	it('Testing errors', () => {
-		expect(() => calculateNumber('NOPE', 2, 2)).to.throw(TypeError);
 		expect(calculateNumber('DIVIDE', 2, 0)).to.equal('Error');
-		expect(() => calculateNumber(2, 'b')).to.throw(TypeError);
-		expect(() => calculateNumber(NaN, 2)).to.throw(TypeError);
 	});
 });
