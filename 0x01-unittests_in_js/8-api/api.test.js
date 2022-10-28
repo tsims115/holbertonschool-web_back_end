f776af718975d53f906fccd3fb8ac9b72a86f5dc
Trenton Sims <tsims115@yahoo.com>
2022-10-27 22:39:51 -0500

    Adding task 8

--- 0x01-unittests_in_js/8-api/api.test.js
+++ 0x01-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,13 @@
 const { expect } = require('chai');
 const request = require('request');
 
-describe('Test the response from app', () => {
-  it('returns the right success message', () => {
-    
-    request('http://localhost:7865', function (error, response, body) {
+describe('test suite', () => {
+  it('test GET', () => new Promise((done) => {
+    request('http://localhost:7865/', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
-      expect(body).to.equal("Welcome to the payment system");
+      expect(body).to.equal('Welcome to the payment system');
+      expect(response.request.method).to.be.equal('GET');
+      done();
     });
-
-  });
+  }));
 });
