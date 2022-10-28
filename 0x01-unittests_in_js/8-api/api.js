f4e599cda25acdf22474fd0b43931960f62a7ade
Trenton Sims <tsims115@yahoo.com>
2022-10-27 22:27:14 -0500

    Adding task 8

--- 0x01-unittests_in_js/8-api/api.js
+++ 0x01-unittests_in_js/8-api/api.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 })
 .listen(7865);
-console. log("API available on localhost port 7865");
+console.log("API available on localhost port 7865");
