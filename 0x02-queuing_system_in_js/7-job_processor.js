d8b561e713c9067402cd406a1b90004d15dd2b01
tsims115 <tsims115@yahoo.com>
2022-11-07 17:39:30 +0000

    Adding task 8 and 9

---
+++ 0x02-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,24 @@
+const kue = require('kue'), queue = kue.createQueue();
+
+const blacklist = ['4153518780', '4153518781'];
+function sendNotification(phoneNumber, message, job, done) {
+  if (blacklist.includes(phoneNumber)) {
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  } else {
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    next(0);
+    done();
+  }
+
+  function next(i) {
+    let slide = phoneNumber;
+    job.progress(i, 2);
+    if (i == 1) done()
+    else next(i + 1);
+  }
+
+}
+
+queue.process('push_notification_code_2', function(job, done){
+    sendNotification(job.data.phoneNumber, job.data.message, job, done);
+  });
